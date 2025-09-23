25217342e19c070d95d409ea7d4956d5688b4e23
ilghar2009 <ilghar.ebrahimi.1388@gmail.com>
2025-09-23 14:17:42 +0330

    change pivot model

--- app/Models/Course_user.php
+++ app/Models/Course_user.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Course_user extends Model
 {
-    //
+    protected $fillable = [
+        'course_id',
+        'user_id',
+    ];
+
 }
--- app/Models/Role_user.php
+++ app/Models/Role_user.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role_user extends Model
 {
-    //
+    protected $fillable = [
+        'role_id',
+        'user_id',
+    ];
 }
