9be9b1cdb7c02117e6563ee1c413fe42c0d67212
ilghar2009 <ilghar.ebrahimi.1388@gmail.com>
2025-10-02 04:30:35 +0330

    lesson update complet

--- app/Http/Controllers/LessonController.php
+++ app/Http/Controllers/LessonController.php
@@ -30,20 +30,24 @@ class LessonController extends Controller
             if($course->user_id === $token->user_id){
 
                 $request->validate([
+                    'course_id' => 'required',
                     'meta_title' => ['sometimes', 'string'],
                     'meta_description' => ['sometimes', 'string'],
                     'video' => ['sometimes', 'mimes:mp4,webm', 'max:20480'],
                     'contents' => ['sometimes', 'string'],
                 ]);
 
-                $path = $request->file('video')->store('videos', 'public');
-                $url = Storage::disk('public')->url($path);
+                //if video exist upload
+                    if($request->file('video')->getSize()) {
+                        $path = $request->file('video')->store('videos', 'public');
+                        $url = Storage::disk('public')->url($path);
+                    }
 
                 //create new record
                     $lesson = Lesson::create([
                         'course_id' => $course->course_id,
-                        'video' => $url,
-                        'contents' => $request->contents,
+                        'video' => $url??null,
+                        'contents' => $request->contents??null,
                         'meta_title' => $request->meta_title??null,
                         'meta_description' => $request->meta_description??null,
                     ]);
@@ -80,7 +84,47 @@ class LessonController extends Controller
      */
     public function update(Request $request, Lesson $lesson)
     {
-        //
+        //check user_id with teacher of course
+
+            //get user with token
+                $tocken_c = $request->bearerToken();
+                $token = Token_User::where('token', $tocken_c)->first();
+
+            //get course
+                $course = Course::where('course_id', $request->course_id)->get();
+
+            if($course->user_id === $token->user_id) {
+
+                $request->validate([
+                    'course_id' => 'sometimes',
+                    'meta_title' => ['sometimes', 'string'],
+                    'meta_description' => ['sometimes', 'string'],
+                    'video' => ['sometimes', 'mimes:mp4,webm', 'max:20480'],
+                    'contents' => ['sometimes', 'string'],
+                ]);
+
+                //if video exist upload
+                    if ($request->file('video')->getSize()) {
+                        $path = $request->file('video')->store('videos', 'public');
+                        $url = Storage::disk('public')->url($path);
+                    }
+
+                $lesson->update([
+                    'course_id' => $course->course_id ?? $lesson->course_id,
+                    'video' => $url ?? $lesson->video,
+                    'contents' => $request->contents ?? $lesson->contents,
+                    'meta_title' => $request->meta_title ?? $lesson->contents,
+                    'meta_description' => $request->meta_description ?? $lesson->meta_description,
+                ]);
+
+                return response()->json([
+                    'lesson' => $lesson,
+                    'message' => 'Lesson updated successfully.'
+                ], 201);
+            }else
+                return response()->json([
+                    'error' => 'you are not authorized to access this page',
+                ], 403);
     }
 
     /**
