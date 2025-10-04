eafc8fe8794d966ad2a89b0e62c62d1c2e3abb55
ilghar2009 <ilghar.ebrahimi.1388@gmail.com>
2025-10-04 18:57:53 +0330

    Enroll a user in the given course.

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,64 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Course;
 use App\Models\Order;
+use App\Models\Token_User;
+use http\Env\Response;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
+    //Enroll a user in the given course.
+    public function enroll(Request $request){
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
+        //get user with token
+          $token_c = $request->bearerToken();
+          $token = Token_User::where('token', $token_c)->first();
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+          $user = $token->user;
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Order $order)
-    {
-        //
-    }
+          $course_id = $request->input('course_id');
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Order $order)
-    {
-        //
-    }
+        //check course exist
+            if(Course::where('course_id', $course_id)->exists()){
+                if(!$user->courses->where('course_id', $course_id)->exist())
+                    $user->courses()->attach($course_id);
+                else
+                    return \response()->json([
+                        'error' => 'You are already enrolled in this course.'
+                    ], 409);
+            }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Order $order)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Order $order)
-    {
-        //
+          return response()->json([
+              'message' => 'User successfully enrolled in this course.',
+          ], 200);
     }
 }
