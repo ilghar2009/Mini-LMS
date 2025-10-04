aab8c64c9131b2ab26ba51a78fb67c7ff4111b95
ilghar2009 <ilghar.ebrahimi.1388@gmail.com>
2025-10-04 18:40:59 +0330

    some change column name

--- database/migrations/2025_09_23_205113_create_orders_table.php
+++ database/migrations/2025_09_23_205113_create_orders_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->string('user_id');
-            $table->string('lesson_id');
+            $table->string('course_id');
             $table->timestamps();
         });
     }
