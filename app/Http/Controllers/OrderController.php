bb096c32b1ffa178b38f8899d257b62a0727ce7c
ilghar2009 <ilghar.ebrahimi.1388@gmail.com>
2025-10-04 19:06:18 +0330

    Unenroll the given user from this course.

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -21,7 +21,7 @@ class OrderController extends Controller
 
           $course_id = $request->input('course_id');
 
-        //check course exist
+        //check if course exist enroll in course
             if(Course::where('course_id', $course_id)->exists()){
                 if(!$user->courses->where('course_id', $course_id)->exist())
                     $user->courses()->attach($course_id);
@@ -35,4 +35,32 @@ class OrderController extends Controller
               'message' => 'User successfully enrolled in this course.',
           ], 200);
     }
+
+    //* Unenroll the given user from this course.
+    public function unenroll(Request $request){
+        //get user with token
+          $token_c = $request->bearerToken();
+          $token = Token_User::where('token', $token_c)->first();
+
+          $user = $token->user;
+
+          $course_id = $request->input('course_id');
+
+        //check if user enroll in this course unenroll
+        if($user->courses->where('course_id', $course_id)->exists()) {
+
+            $user->courses()
+                ->where('course_id', $course_id)
+                ->detach($course_id);
+
+            return response()->json([
+                'message' => 'User successfully unenrolled in this course.'
+            ], 200);
+
+        }else
+            return \response()->json([
+                'error' => 'You are not enrolled in this course.'
+            ], 404);
+    }
+
 }
